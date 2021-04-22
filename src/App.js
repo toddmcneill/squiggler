06cd7d9bede89d05a27e4b0ceca97959c61df21f
Todd McNeill <todd-mcneill@pluralsight.com>
2021-04-22 15:19:46 -0600

    Good layout
    * Add Gugi font
    * Set color theme
    * Position button and drawing area
    * Style header
    * Add logo squiggle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import styles from './App.module.css'
-import AppBar from '@material-ui/core/AppBar';
+import AppBar from '@material-ui/core/AppBar'
 import Main from './components/Main'
+import Logo from './components/Logo'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4DD0E1',
+      dark: '#00ACC1'
+
+    },
+    secondary: {
+      main: '#FFEB3B',
+      dark: '#FDD835'
+    }
+  },
+})
 
 export default function App() {
   return (
-    <div className={styles.app}>
-      <div className={styles.header}>
-        <AppBar color='primary'>Squiggler</AppBar>
+    <ThemeProvider theme={theme}>
+      <div className={styles.app}>
+        <div className={styles.header}>
+          <AppBar color='white' position='relative' className={styles.appBar}>
+            <div className={styles.headerText}>
+              <Logo />
+              squiggler
+            </div>
+          </AppBar>
+        </div>
+        <div className={styles.main}>
+          <Main />
+        </div>
       </div>
-      <div className={styles.main}><Main /></div>
-    </div>
+    </ThemeProvider>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import Fab from '@material-ui/core/Fab'
+import Container from '@material-ui/core/Container'
 import AddIcon from '@material-ui/icons/Add'
 import Canvas from './Canvas'
 
@@ -6,14 +7,16 @@ import styles from './Main.module.css'
 
 export default function Main() {
   return (
-    <div className={styles.main}>
+    <Container className={styles.container}>
       <div className={styles.drawingArea}>
+        <Canvas />
+      </div>
+      <div className={styles.buttonContainer}>
         <Fab variant='extended' color='secondary'>
           <AddIcon />
           New Squiggle
         </Fab>
-        <Canvas />
       </div>
-    </div>
+    </Container>
   )
 }
