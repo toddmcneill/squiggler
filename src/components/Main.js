8fce320bec840b15472d5a0b72ecaf1b47b6b857
Todd McNeill <todd-mcneill@pluralsight.com>
2021-04-23 14:44:25 -0600

    Start a squiggle on page load

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect, useCallback } from 'react'
 import Fab from '@material-ui/core/Fab'
 import Container from '@material-ui/core/Container'
 import AddIcon from '@material-ui/icons/Add'
@@ -63,31 +63,28 @@ function drawRandomCurve(ctx, width, height, { startX = null, startY = null, sta
   return { startX: endX, startY: endY, startBezierX: 2 * endX - endBezierX, startBezierY: 2 * endY - endBezierY }
 }
 
-export function getMirrorPoint(pivotPoint, pointToMirror) {
-  return { x: 2 * pivotPoint.x - pointToMirror.x, y: 2 * pivotPoint.y - pointToMirror.y }
-}
+// Values between 5 and 10 seem to work best. Too high, and it doesn't smooth much. Too low, and it makes swoopy artifacts.
+const SMOOTHING_FACTOR = 6
 
 export function getFirstControlPoint({ a, b, c}) {
-  const x = b.x + (c.x - a.x) / 6
-  const y = b.y + (c.y - a.y) / 6
+  const x = b.x + (c.x - a.x) / SMOOTHING_FACTOR
+  const y = b.y + (c.y - a.y) / SMOOTHING_FACTOR
   return { x, y }
 }
 
 export function getSecondControlPoint({ b, c, d}) {
-  const x = c.x + (b.x - d.x) / 6
-  const y = c.y + (b.y - d.y) / 6
+  const x = c.x + (b.x - d.x) / SMOOTHING_FACTOR
+  const y = c.y + (b.y - d.y) / SMOOTHING_FACTOR
   return { x, y }
 }
 
 function smoothLine(ctx, { a, b, c, d }) {
+  // Draw a line between B and C. Use the slope of the surrounding points to calculate control points.
   ctx.beginPath()
   ctx.moveTo(b.x, b.y)
-  // ctx.lineTo(c.x, c.y)
   const firstControl = getFirstControlPoint({ a, b, c })
   const secondControl = getSecondControlPoint({ b, c, d })
   ctx.bezierCurveTo(firstControl.x, firstControl.y, secondControl.x, secondControl.y, c.x, c.y)
-  console.log('Point', a, b, c)
-  // console.log('Bezier Curve', 2 * b.x - a.x, 2 * b.y - a.y, c.x - (d.x - c.x), c.y - (d.y - d.y), c.x, c.y)
   ctx.strokeStyle = 'black'
   ctx.lineWidth = 3
   ctx.lineCap = 'smooth'
@@ -101,23 +98,47 @@ function getPoint (sketchElement, event) {
 
 export default function Main() {
   const [mouseDown, setMouseDown] = useState(false)
-  const [pointState, setPointState] = useState()
+  const [pointState, setPointState] = useState({})
   const sketchElement = useRef()
 
-  const startNewSquiggle = () => {
-    // Scale canvas to 1:1 with pixel size.
+  const fixCanvasScale = () => {
     const canvas = sketchElement.current
     const { width, height } = canvas.getBoundingClientRect()
     canvas.setAttribute('width', width)
     canvas.setAttribute('height', height)
-    const ctx = canvas.getContext('2d')
+    return { width, height }
+  }
 
-    // clear canvas
+  const startNewSquiggle = useCallback(() => {
+    // Scale canvas to 1:1 with pixel size.
+    const canvas = sketchElement.current
+    const { width, height } = fixCanvasScale()
+
+    // Clear canvas
+    const ctx = canvas.getContext('2d')
     ctx.clearRect(0, 0, width, height)
 
-    // draw squiggle
+    // Draw squiggle
     drawRandomSquiggle(ctx, width, height)
-  }
+  }, [sketchElement])
+
+  // Reset the canvas on window resize to avoid weird offset issues.
+  // It's not the optimal solution, but prevents a pretty confusing bug.
+  // useEffect(() => {
+  //   const resizeHandler = () => {
+  //     fixCanvasScale()
+  //     startNewSquiggle()
+  //   }
+  //
+  //   window.addEventListener('resize', resizeHandler)
+  //   return () => {
+  //     window.removeEventListener('resize', resizeHandler)
+  //   }
+  // })
+
+  useEffect(() => {
+    startNewSquiggle()
+  }, [startNewSquiggle])
 
   const onMouseDown = (event) => {
     setMouseDown(true)
